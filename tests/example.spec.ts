b0e24d1fb0b660baa8532547e0b3caf898b54018
Anton Nazarov <a.nazarov3@g.nsu.ru>
2026-04-09 15:18:53 +0700

    remove default tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,35 +1,7 @@
 import { test, expect } from '@playwright/test';
-import { PlaywrightPage } from './playwrightPage';
 import { AuthPage } from './auth-page';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
-test('user can search through the documentation', async ({ page }) => {
-  const playwrightPage = new PlaywrightPage(page);
-  await playwrightPage.load();
-
-  await playwrightPage.searchButton.click();
-  await playwrightPage.searchInput.fill('Fixtures');
-
-  await expect(playwrightPage.searchHitsGroup).not.toHaveCount(0);
-});
-
-test('user can sign in', async ({ page }) => {
+test.only('user can sign in', async ({ page }) => {
   const authPage = new AuthPage(page);
   await authPage.load();
 
